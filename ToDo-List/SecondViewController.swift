52f6a2b5f731722b30fe4e40785b77f6670d4181
ki11er <deathrazer007@yahoo.com>
2015-10-29 13:13:45 -0300

    Initial Commit

---
+++ ToDo-List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToDo-List
+//
+//  Created by Priom on 2015-10-29.
+//  Copyright © 2015 Priom.net. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
