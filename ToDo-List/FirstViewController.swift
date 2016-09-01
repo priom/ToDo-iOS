c679c6b881516ea3c70bc2df5e46692a0c355a56
Priom Chowdhury <priom.galaxy@gmail.com>
2016-09-01 19:39:08 -0300

    complete project published

--- ToDo-List/FirstViewController.swift
+++ ToDo-List/FirstViewController.swift
@@ -6,13 +6,23 @@
 //  Copyright © 2015 Priom.net. All rights reserved.
 //
 
+//View your todo page
 import UIKit
 
-class FirstViewController: UIViewController {
+//Global variable for todo list for total app
+var tdList = [String]()
+
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet var listTbl: UITableView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if (UserDefaults.standard.object(forKey: "tdList") != nil) {
+            tdList = UserDefaults.standard.object(forKey: "tdList") as! [String]
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,26 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tdList.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = tdList[(indexPath as NSIndexPath).row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            tdList.remove(at: (indexPath as NSIndexPath).row)
+            UserDefaults.standard.set(tdList, forKey: "tdList")
+            listTbl.reloadData()
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        listTbl.reloadData()
+    }
 }
 
--- ToDo-List/SecondViewController.swift
+++ ToDo-List/SecondViewController.swift
@@ -6,10 +6,30 @@
 //  Copyright © 2015 Priom.net. All rights reserved.
 //
 
+//Add New ToDo page
 import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var todoTxt: UITextField!
+
+    @IBAction func addBtn(_ sender: AnyObject) {
+        tdList.append(todoTxt.text!)
+        todoTxt.text = ""
+        UserDefaults.standard.set(tdList, forKey: "tdList")
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField!) -> Bool {
+        todoTxt.resignFirstResponder()
+        return true
+    }
+
+
+// Default func
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
